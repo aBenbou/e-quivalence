a550b984b1a15a883f71df0f7cfac217858b6cec
aBenbou <aymane.benbouzid@outlook.com>
2024-04-16 01:24:47 +0100

    added frontend login+register

--- pi2024-front/src/app/app.module.ts
+++ pi2024-front/src/app/app.module.ts
@@ -3,15 +3,37 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { CardModule } from 'primeng/card';
+import { AppRoutingModule } from './app-routing.module';
+import { InputTextModule } from 'primeng/inputtext';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MessageService } from 'primeng/api';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule
+    BrowserModule, 
+    AppRoutingModule, 
+    HttpClientModule, 
+    CardModule, 
+    InputTextModule, 
+    ReactiveFormsModule,
+    ButtonModule,
+    ToastModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
